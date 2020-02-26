eff83cf5d0f96aa3c6bc81f9f925d10911b02b79
malte9202 <malte.niedereichholz@gmail.com>
2020-02-26 21:44:12 +0100

    rewrite data part

---
+++ data.py
@@ -0,0 +1,80 @@
+import sqlite3  # import sqlite3 module
+from sqlite3 import Error  # import sqlite3 errors
+from Activity import Activity  # import activity class
+
+connection = sqlite3.connect("activity_tracker.db")  # enable db connection
+
+cursor = connection.cursor()  # set cursor
+
+
+def create_table_activities():  # function to create table
+    # sql statement to create table activities
+    create_table_query = 'CREATE TABLE IF NOT EXISTS activities' \
+                         '(id INTEGER PRIMARY KEY,' \
+                         'date TEXT,' \
+                         'type TEXT,' \
+                         'distance REAL,' \
+                         'duration REAL,' \
+                         'average_speed REAL,' \
+                         'info TEXT)'
+    cursor.execute(create_table_query)  # execute query to create table
+    connection.commit()  # commit queries
+
+
+def insert_activity(activity):  # function to insert activity
+    insert_query = 'INSERT INTO activities VALUES (?, ?, ?, ?, ?, ?, ?)'  # statement for inserting activities
+    # execute insert query
+    cursor.execute(insert_query, (id, activity.date, activity.type, activity.distance,
+                                  activity.duration, activity.average_speed, activity.info))
+    connection.commit()  # commit queries
+
+
+def activity_overview():  # function to show recent activities
+    overview_query = 'SELECT * FROM activities LIMIT LIMIT 50'  # query to show recent activities
+    cursor.execute(overview_query)  # execute overview query
+    connection.commit()  # commit query
+
+
+def search_for_date(date):  # function to search for date
+    search_query_date = 'SELECT * FROM activities WHERE date = ? LIMIT 50'  # query to search for date
+    cursor.execute(search_query_date, date)  # execute search for date
+    connection.commit()  # commit query
+
+
+def search_for_type(type):  # function to search for type
+    search_query_type = 'SELECT * FROM activities WHERE type LIKE ? LIMIT 50'
+    cursor.execute(search_query_type, type)
+    connection.commit()
+
+
+def search_for_distance(distance):  # function to search for distance
+    search_query_distance = 'SELECT * FROM activities WHERE distance = ? LIMIT 50'
+    cursor.execute(search_query_distance, distance)
+    connection.commit()
+
+
+def search_for_duration(duration):  # function to search for duration
+    search_query_duration = 'SELECT * FROM activities WHERE duration = ? LIMIT 50'
+    cursor.execute(search_query_duration, duration)
+    connection.commit()
+
+
+def search_for_speed(speed):  # function to search for speed
+    search_query_speed = 'SELECT * FROM activities WHERE average_speed = ? LIMIT 50'
+    cursor.execute(search_query_speed, speed)
+    connection.commit()
+
+
+def search_for_info(info):  # function to search for info
+    search_query_info = 'SELECT * FROM activities WHERE info LIKE ? LIMIT 50'
+    cursor.execute(search_query_info, info)
+    connection.commit()
+
+
+def order_activities(field):  # function to order results
+    order_query = 'SELECT * FROM activities ORDER BY ? LIMIT 50'
+    cursor.execute(order_query, field)
+    connection.commit()
+
+
+connection.close()  # close connection
--- database.py
+++ database.py
@@ -1,34 +1,15 @@
-import sqlite3
-from sqlite3 import Error
-from Activity import Activity
 
-connection = sqlite3.connect("activity_tracker.db")
-cursor = connection.cursor()
+# queries for ordering by each db field
+order_activity_by_date = 'SELECT * FROM activities ORDER BY date LIMIT 10'
+order_activity_by_type = 'SELECT * FROM activities ORDER BY type LIMIT 10'
+order_activity_by_distance = 'SELECT * FROM activities ORDER BY distance LIMIT 10'
+order_activity_by_duration = 'SELECT * FROM activities ORDER BY duration LIMIT 10'
+order_activity_by_speed = 'SELECT * FROM activities ORDER BY average_speed LIMIT 10'
+order_activity_by_info = 'SELECT * FROM activities ORDER BY additional_info LIMIT 10'
 
-create_table_activities = 'CREATE TABLE IF NOT EXISTS activities' \
-                          '(id INTEGER PRIMARY KEY,' \
-                          'date TEXT,' \
-                          'type TEXT,' \
-                          'distance REAL,' \
-                          'duration REAL,' \
-                          'average_speed REAL,' \
-                          'additional_info TEXT)'
+connection.commit()  # commit queries
 
-cursor.execute(create_table_activities)
-
-activity_insert = 'INSERT INTO activities VALUES (?, ?, ?, ?, ?, ?, ?)'
-
-activity_1 = Activity('2020-02-25', 'strength', None, 61, None, 'mixed')
-
-cursor.execute(activity_insert, (23, activity_1.date, activity_1.type, activity_1.distance, activity_1.duration, activity_1.average_speed, activity_1.info))
-
-#cursor.execute(activity_insert, (1, '2020-02-24', 'run', 10, 60, 10, 'indoor'))
-
-cursor.execute("SELECT * FROM activities LIMIT 20")
-
-connection.commit()
-
-print(cursor.fetchall())
+print(cursor.fetchall())  # fetch all results
 
 connection.commit()
 
