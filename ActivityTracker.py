805a1a312c7b9763c78d2f28ee3811ac8a12380b
malte9202 <malte.niedereichholz@gmail.com>
2020-03-22 09:50:38 +0100

    rewrite step by step in new file

--- ActivityTracker.py
+++ ActivityTracker.py
@@ -32,6 +32,11 @@ def insert_activity(activity):
     connection.commit()  # commit query
 
 
+# test function for button action
+def test_function():
+    intro_label.config(text="activity saved")
+
+
 # create program flow and graphical user interface
 
 window = Tk()  # create main window
@@ -55,3 +60,35 @@ input_distance = Entry(window, bd=3, width=15)
 input_duration = Entry(window, bd=3, width=15)
 input_average_speed = Entry(window, bd=3, width=15)
 input_info = Entry(window, bd=3, width=15)
+
+# create buttons
+exit_button = Button(window, text="Quit", command=window.quit)
+save_activity_button = Button(window, text="Save activity", command=test_function)
+
+# add components to the window in grid mode
+intro_label.grid(row=0, column=0, columnspan=2)  # info label
+# date elements
+date_label.grid(row=1, column=0)
+input_date.grid(row=1, column=1)
+# type elements
+type_label.grid(row=2, column=0)
+input_type.grid(row=2, column=1)
+# distance elements
+distance_label.grid(row=3, column=0)
+input_distance.grid(row=3, column=1)
+# duration elements
+duration_label.grid(row=4, column=0)
+input_duration.grid(row=4, column=1)
+# average speed elements
+average_speed_label.grid(row=5, column=0)
+input_average_speed.grid(row=5, column=1)
+# info elements
+info_label.grid(row=6, column=0)
+input_info.grid(row=6, column=1)
+# add activity button
+save_activity_button.grid(row=7, column=1)
+# exit button
+exit_button.grid(row=7, column=3)
+
+# waiting for user input in main loop
+window.mainloop()
